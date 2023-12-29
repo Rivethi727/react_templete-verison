e7f902f15ceea4109a41e0849f79717d1d71e9c8
Rivethika <nivethanivi849@gmail.com>
2023-12-29 11:34:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './assets/css/style.css';
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+// import Button from './Button';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div >
+     
+   <Header/>
+   <Body/>
+   <Footer/>
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,25 @@
+
+
+function Button() {
+  return (
+    <div class="Text">
+        <button>Submit</button>
+        <Text></Text>
+        </div>
+    
+   
+  );
+}
+
+function Text() {
+    return (
+    <div class="Text">
+        hellllooo
+    </div>
+
+    );
+  }
+
+export default Button;
+
+
